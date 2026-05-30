dd4febef3c831ebe75c3c5d74ee229fda340397a
Krish Prajapati <kprajapati0806@gmail.com>
2026-05-29 19:28:07 -0500

    feat: add global exception handler and ApiError response DTO

---
+++ support-api/src/main/java/com/krish/supportapi/exception/ApiError.java
@@ -0,0 +1,24 @@
+package com.krish.supportapi.exception;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError {
+
+    private LocalDateTime timestamp;
+
+    private int status;
+
+    private String error;
+
+    private String message;
+
+    private String path;
+}
